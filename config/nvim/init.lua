3d2f7c89ea7b311bf1b5d450b305634d3aeca590
DanDeanDean <dpd5518@psu.edu>
2024-09-09 06:59:40 -0400

    update nvim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -555,13 +555,32 @@ require("lazy").setup({
 				gopls = {},
 				pyright = {},
 				rust_analyzer = {},
+				yamlls = {
+					settings = {
+						yaml = {
+							schemaStore = {
+								enable = false,
+								url = "",
+							},
+							-- Set up Pipeline tooling
+							schemas = {
+								--["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.yml",
+								--["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "ci/*.yml",
+								["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] = "docker-compose*.{yml,yaml}",
+								--["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "BuildTools/*.{yml,yaml}",
+								-- These schemas aren't working, so we need to debug locally
+								["/Users/dandean/git/CNPPipe/schema/schema.json"] = "YamlDefinitions/*.{yaml,yml}",
+							},
+						},
+					},
+				},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				-- tsserver = {},
+				tsserver = {},
 				--
 
 				lua_ls = {
