04eaaa80081d7ecda2dde50a2c6fc512162f00a3
dandeandean <DanDean44523@gmail.com>
2024-11-19 19:27:11 -0500

    home manage a little

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -1,10 +1,7 @@
-# home-manager switch
 { config, pkgs, ...} : {
   home.username = "ddd";
   home.homeDirectory = "/home/ddd";
   home.stateVersion = "24.05";
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   programs.git = {
     enable = true;
@@ -15,20 +12,32 @@
     enable = true;
     clock24 = true;
     plugins = with pkgs.tmuxPlugins; [
-        sensible
         yank
         {
           plugin = dracula;
           extraConfig = ''
-            set -g @dracula-show-battery false
-            set -g @dracula-show-powerline true
-            set -g @dracula-refresh-rate 10
+            set -g @dracula-plugins "battery time git"
+            set -g @dracula-time-format "%A %R"
+            set -g @dracula-show-left-icon '🧛'
+            set -g @dracula-battery-label '🩸'
             '';
         }
     ];
-
     extraConfig = ''
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+      set -s escape-time 0
+      unbind C-b
+      set-option -g prefix C-l
+      bind-key C-l send-prefix
+      bind \\ split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      unbind '"'
+      unbind %
       set -g mouse on
+      set-window-option -g mode-keys vi
       '';
   };
 }
