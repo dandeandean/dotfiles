2802d00c1ea00feeb741c15ae33b482d0ae8a8ce
Dan Dean <dandean44523@gmail.com>
2024-12-07 17:16:09 -0500

    nvim update

--- config/nvim/lua/lazy/lspconfig.lua
+++ config/nvim/lua/lazy/lspconfig.lua
@@ -6,13 +6,13 @@ return {
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			{ "j-hui/fidget.nvim", opts = {} },
-
 			{ "folke/neodev.nvim", opts = {} },
 		},
 		config = function()
 			require("custom.plugins.lsp-attach")
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+			capabilities = vim.tbl_deep_extend("force", capabilities,
+				require("cmp_nvim_lsp").default_capabilities())
 			local servers = {
 				clangd = {},
 				gopls = {},
@@ -28,8 +28,10 @@ return {
 								url = "",
 							},
 							-- Set up Pipeline tooling
+							diagnostics = { disable = { 'missing-fields' } },
 							schemas = {
-								["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] = "docker-compose*.{yml,yaml}",
+								["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] =
+								"docker-compose*.{yml,yaml}",
 								["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
 									"BuildTools/*.{yml,yaml}",
 									"YamlDefinitions/*.{yml,yaml}",
@@ -39,13 +41,14 @@ return {
 						},
 					},
 				},
-
 				lua_ls = {
 					-- cmd = {...},
 					-- filetypes { ...},
 					-- capabilities = {},
 					settings = {
 						Lua = {
+							indent_style = "space",
+							indent_size = "2",
 							completion = {
 								callSnippet = "Replace",
 							},
@@ -67,15 +70,14 @@ return {
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format lua code
+				-- "stylua", -- Used to format lua code
 				"rust_analyzer", -- Used to format Rust code
 				"pyright", -- Used to format python code
 				"clangd", -- Used to format c pepe
 				"gopls", -- Used for golang
-				"helm-ls", -- Used for golang
+				"helm-ls", -- Used for helm
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
 			require("mason-lspconfig").setup({
 				handlers = {
 					function(server_name)
@@ -83,7 +85,8 @@ return {
 						-- This handles overriding only values explicitly passed
 						-- by the server configuration above. Useful when disabling
 						-- certain features of an LSP (for example, turning off formatting for tsserver)
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities,
+							server.capabilities or {})
 						require("lspconfig")[server_name].setup(server)
 					end,
 				},
--- config/nvim/lua/lazy/tree-sitter.lua
+++ config/nvim/lua/lazy/tree-sitter.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":tsupdate",
 		opts = {
-			ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
+			ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc", "gotmpl", "helm" },
 			-- autoinstall languages that are not installed
 			auto_install = true,
 			highlight = { enable = true },
@@ -11,10 +11,8 @@ return {
 		},
 		config = function(_, opts)
 			-- [[ configure treesitter ]] see `:help nvim-treesitter`
-
 			---@diagnostic disable-next-line: missing-fields
 			require("nvim-treesitter.configs").setup(opts)
-
 			-- there are additional nvim-treesitter modules that you can use to interact
 			-- with nvim-treesitter. you should go explore a few and see what interests you:
 			--
