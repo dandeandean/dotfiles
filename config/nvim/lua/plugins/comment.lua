c1f11c44323c5802097d407fe61e3fe78404a568
Dan Dean <dandean44523@gmail.com>
2024-12-08 15:04:58 -0500

    hot fix

---
+++ config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			opleader = {
+				line = "<leader>c",
+				block = "<leader>b",
+			},
+		},
+	},
+}
---
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,86 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			{ "j-hui/fidget.nvim", opts = {} },
+			{ "folke/neodev.nvim", opts = {} },
+		},
+		config = function()
+			require("custom.plugins.lsp-attach")
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+			local servers = {
+				clangd = {},
+				gopls = {},
+				pyright = {},
+				rust_analyzer = {},
+				yamlls = {
+					settings = {
+						yaml = {
+							diagnostics = {
+								disable = {
+									"missing-fields",
+								},
+							},
+							schemas = {
+								["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] = "docker-compose*.{yml,yaml}",
+								["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
+									"BuildTools/*.{yml,yaml}",
+									"YamlDefinitions/*.{yml,yaml}",
+									"YamlTemplates/*.{yml,yaml}",
+								},
+							},
+						},
+					},
+				},
+				lua_ls = {
+					settings = {
+						Lua = {
+							indent_style = "space",
+							indent_size = "2",
+							completion = {
+								callSnippet = "Replace",
+							},
+						},
+					},
+				},
+			}
+
+			-- Ensure the servers and tools above are installed
+			--  To check the current status of installed tools and/or manually install
+			--  other tools, you can run
+			--    :Mason
+			--
+			--  You can press `g?` for help in this menu
+			require("mason").setup()
+
+			-- You can add other tools here that you want Mason to install
+			-- for you, so that they are available from within Neovim.
+			local ensure_installed = vim.tbl_keys(servers or {})
+			vim.list_extend(ensure_installed, {
+				"stylua", -- Used to format lua code
+				"rust_analyzer", -- Used to format Rust code
+				"pyright", -- Used to format python code
+				"clangd", -- Used to format c pepe
+				"gopls", -- Used for golang
+				"helm-ls", -- Used for helm
+			})
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+			require("mason-lspconfig").setup({
+				handlers = {
+					function(server_name)
+						local server = servers[server_name] or {}
+						-- This handles overriding only values explicitly passed
+						-- by the server configuration above. Useful when disabling
+						-- certain features of an LSP (for example, turning off formatting for tsserver)
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						require("lspconfig")[server_name].setup(server)
+					end,
+				},
+			})
+		end,
+	},
+}
