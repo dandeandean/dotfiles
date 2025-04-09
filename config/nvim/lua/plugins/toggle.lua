cae28b1ec5eb47e5b1f3bc1a3e06e4880b411691
DanDeanDean <dpd5518@psu.edu>
2025-04-09 07:03:28 -0400

    add toggle nvim

---
+++ config/nvim/lua/plugins/toggle.lua
@@ -0,0 +1,7 @@
+return {
+	-- amongst your other plugins
+	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+	-- or
+	-- TODO: Configure to get cwd & open there
+	--{'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
+}
