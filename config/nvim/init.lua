be0736f269320005638da0369c95dea9f9adf630
DanDeanDean <dpd5518@psu.edu>
2025-04-11 07:02:29 -0400

    add back gruv

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -23,7 +23,6 @@
 --]]
 
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.cmd.colorscheme("retrobox")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
