0b0de55bb803f00aae58a75dab99b0b717f53d21
DanDeanDean <dpd5518@psu.edu>
2025-04-14 06:43:44 -0400

    add some more snippets

--- config/nvim/lua/plugins/autocomplete.lua
+++ config/nvim/lua/plugins/autocomplete.lua
@@ -22,7 +22,7 @@ return {
 			-- See `:help cmp`
 			require("luasnip.loaders.from_vscode").lazy_load()
 			require("luasnip.loaders.from_vscode").load_standalone({
-				path = "~/.config/nvim/lua/plugins/snippets/pipelines.code-snippets",
+				path = "~/.config/nvim/lua/plugins/snippets/pipelines.code-snippets.json",
 			})
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
