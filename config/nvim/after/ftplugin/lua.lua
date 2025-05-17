4eaf0580863f8ad96037051cf5a69c39a05dc386
DanDeanDean <dpd5518@psu.edu>
2025-05-17 12:57:18 -0400

    lua dev

--- config/nvim/after/ftplugin/lua.lua
+++ config/nvim/after/ftplugin/lua.lua
@@ -1,3 +1,19 @@
 local set = vim.opt_local
 
 set.shiftwidth = 2
+
+vim.lsp.config('lua_ls', {
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				globals = {
+					'vim',
+					'require',
+				},
+			},
+		},
+	},
+})
--- config/nvim/after/ftplugin/yaml.lua
+++ config/nvim/after/ftplugin/yaml.lua
@@ -1,4 +1,19 @@
 local set = vim.opt_local
+vim.lsp.config('yamlls', {
+	settings = {
+		yaml = {
+			diagnostics = {
+				disable = {
+					"missing-fields",
+				},
+			},
+			schemas = {
+				["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] =
+				"docker-compose*.{yml,yaml}",
+			},
+		},
+	},
+})
 
 set.number = true
 set.relativenumber = true
