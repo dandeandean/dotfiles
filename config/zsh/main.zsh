d5e4038bccb8169151eff3969b8b5681f48a4c93
DanDeanDean <dpd5518@psu.edu>
2025-06-08 11:32:11 -0400

    format

--- config/zsh/main.zsh
+++ config/zsh/main.zsh
@@ -3,7 +3,7 @@ source ./alias.zsh
 source ./func.zsh
 
 if command -v zoxide &>/dev/null; then
-				eval "$(zoxide init zsh)"
+  eval "$(zoxide init zsh)"
 fi
 # TMUX on startup
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
