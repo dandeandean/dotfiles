e4547cf514b16761275c150b6f657ce8d5651cc0
DanDeanDean <dpd5518@psu.edu>
2025-06-08 11:32:27 -0400

    more

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -3,12 +3,12 @@ export ZSH_THEME=bira
 export CONFIG_PATH=bira
 
 if command -v go &> /dev/null; then
-				export PATH="$PATH:$(go env GOPATH)/bin"
+	export PATH="$PATH:$(go env GOPATH)/bin"
 fi
 
 if command -v zk &> /dev/null; then
-				export ZK_NOTEBOOK_DIR="$HOME/Notes/"
+	export ZK_NOTEBOOK_DIR="$HOME/Notes/"
 fi
 if command -v cards &> /dev/null; then
-				export CARDSHOME="$HOME/.config/cards"
+	export CARDSHOME="$HOME/.config/cards"
 fi
