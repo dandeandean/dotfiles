4cda90a96ec6ae1c055b97d0cc256a7e8b544849
DanDeanDean <dpd5518@psu.edu>
2025-09-07 12:41:11 -0400

    ignore warnings on gd

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -10,6 +10,22 @@ vim.diagnostic.config({
 	update_in_insert = false,
 	severity_sort = false,
 })
+
+-- From: https://github.com/nvim-telescope/telescope.nvim/issues/3439
+local notify_original = vim.notify
+vim.notify = function(msg, ...)
+	if
+			msg
+			and (
+				msg:match 'position_encoding param is required'
+				or msg:match 'Defaulting to position encoding of the first client'
+				or msg:match 'multiple different client offset_encodings'
+			)
+	then
+		return
+	end
+	return notify_original(msg, ...)
+end
 -- lua/
 require("options")
 require("keymappings")
