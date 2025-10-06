f982764b26cf8917ff956a2fe4eda9c0f469b07d
dandeandean <dandean44523@gmail.com>
2025-10-06 10:24:18 +0000

    remove wallpaper

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -1,9 +1,10 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   home.username = "ddd";
   home.homeDirectory = "/home/ddd";
   home.stateVersion = "25.05"; # Please read the comment before changing.
   home.packages = with pkgs; [
+    home-manager
     ################### User ###################
     fastfetch
     htop
@@ -19,6 +20,9 @@
     opentofu
     k3d
     k9s
+    jq
+    tree
+    fzf
 
     ################### NVIM DEPS ###################
     ripgrep
@@ -42,6 +46,37 @@
     userEmail = "dandean44523@gmail.com";
   };
   fonts.fontconfig.enable = true;
+
+  # nix = {
+  #   package = pkgs.nix;
+  #   settings.experimental-features = [ "nix-command" "flakes" ];
+  # };
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      v = "nvim";
+      reload = "source $HOME/.zshrc";
+      config = "nvim $HOME/.config/";
+      manage = "nvim $HOME/.config/home-manager/";
+      nixedit = "sudoedit /etc/nixos/configuration.nix";
+      nixupdate = "sudo nixos-rebuild switch";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+    };
+    initExtra = ''
+      if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+	exec tmux
+      fi
+      export ZK_NOTEBOOK_DIR="$HOME/notes"
+      export EDITOR="nvim"
+      source $HOME/git/dotfiles/config/zsh/main.zsh
+    '';
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
