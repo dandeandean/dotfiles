dd2b522ff1e0256888e4a3350a9a9b2f1af020fd
DanDeanDean <dpd5518@psu.edu>
2025-12-14 11:19:42 -0500

    get rid of that

--- config/nvim/lua/custom/plugins/bookworm.lua
+++ config/nvim/lua/custom/plugins/bookworm.lua
@@ -3,7 +3,6 @@ local M = {}
 -- For some reason the open ... won't actually spawn a browser
 -- unless we sleep... DUMB DUMB DUMB
 local BW_CMD = "bookworm && sleep 0.5"
-local LG_CMD = "lazygit"
 local Terminal = require("toggleterm.terminal").Terminal
 
 function M.bookworm()
@@ -21,23 +20,24 @@ function M.bookworm()
 	BookwormTerm:toggle()
 end
 
-function M.lazygit()
-	local BookwormTerm = Terminal:new({
-		cmd = LG_CMD,
-		hidden = false,
-		close_on_exit = true,
-		direction = "float",
-		display_name = "Lazygit",
-		float_opts = {
-			border = "curved",
-			title_pos = "center",
-		},
-	})
-	BookwormTerm:toggle()
-end
-vim.keymap.set("n", "<leader>g", M.lazygit, {
-	desc = "Run Lazygit",
-})
+-- local LG_CMD = "lazygit"
+-- function M.lazygit()
+-- 	local BookwormTerm = Terminal:new({
+-- 		cmd = LG_CMD,
+-- 		hidden = false,
+-- 		close_on_exit = true,
+-- 		direction = "float",
+-- 		display_name = "Lazygit",
+-- 		float_opts = {
+-- 			border = "curved",
+-- 			title_pos = "center",
+-- 		},
+-- 	})
+-- 	BookwormTerm:toggle()
+-- end
+-- vim.keymap.set("n", "<leader>g", M.lazygit, {
+-- 	desc = "Run Lazygit",
+-- })
 
 vim.api.nvim_create_user_command("Bookworm", M.bookworm, { nargs = "*" })
 
