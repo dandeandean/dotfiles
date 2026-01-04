3cd4893d3b5dc7681fd00c43140c559c568e0c4b
dandeandean <dandean44523@gmail.com>
2026-01-04 12:56:27 -0500

    simple ts

--- config/nvim/lua/plugins/tree-sitter.lua
+++ config/nvim/lua/plugins/tree-sitter.lua
@@ -3,25 +3,10 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		opts = {
-			ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc", "gotmpl", "helm" },
+			ensure_installed = { "lua" },
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
 		},
-		config = function(_, opts)
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup(opts)
-		end,
 	},
-	-- from https://github.com/ngalaiko/tree-sitter-go-template
-	vim.filetype.add({
-		extension = {
-			gotmpl = "gotmpl",
-		},
-		pattern = {
-			[".*/templates/.*%.tpl"] = "helm",
-			[".*/templates/.*%.ya?ml"] = "helm",
-			["helmfile.*%.ya?ml"] = "helm",
-		},
-	}),
 }
